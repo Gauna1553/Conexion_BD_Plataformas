275140ac6447e6b01ecb25dceef0d9a16f5391a0
Gauna1553 <gabrielgauna04@gmail.com>
2023-08-31 18:19:38 -0300

    comente el codigo

--- index.js
+++ index.js
@@ -58,14 +58,16 @@ const crearUsuarios = async  (request, response) => {
 app.get('/api/usuarios', crearUsuarios);
 
 
-/*const users = await User.findAll();
+/*
+const users = await User.findAll();
 const user = await User.findByPk(1);
 
 const usuarios = await user.findAll({
     where: {
         age: { [Op.gte]: 18} //Usuarios mayores o iguales a 18
     }
-})*/
+})
+*/
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
